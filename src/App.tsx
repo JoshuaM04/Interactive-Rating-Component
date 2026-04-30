99d341426590e1f945b8d9002c93587508b18aa2
Viper <pruncle21@gmail.com>
2026-04-30 17:13:34 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Header from './components/Header.tsx';
+import Main from './components/Main.tsx';
+import Footer from './components/Footer.tsx';
+
+export default function App() {
+  return (
+    <>
+      How did we do?
+
+      Please let us know how we did with your support request. All feedback is appreciated 
+      to help us improve our offering!
+
+      1 2 3 4 5
+
+      Submit
+
+      Thank you!
+
+      We appreciate you taking the time to give a rating. If you ever need more support, 
+      don’t hesitate to get in touch!
+
+      
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </footer>
+    </>
+  )
+}
