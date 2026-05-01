0e86ea530bcf9918bab3453139ed6322e7788d12
Viper <pruncle21@gmail.com>
2026-05-01 12:02:12 -0500

    Imported react-aria components to handle accessibility edge-cases and handle the modal pop-up. Adjusted some of the layout configurations, refined the custom utility classes via the theme directive in App.css, refined where the modal shows up, and adjusted hover/active states for buttons. Ensured there is dynamic styling and conditonal rendering.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,6 @@
 import starIcon from '../assets/images/icon-star.svg';
+import illustrationThankYou from '../assets/images/illustration-thank-you.svg';
+import { DialogTrigger, Modal, Dialog, Button } from 'react-aria-components';
 import { useState } from 'react';
 
 interface MobileProps {
@@ -14,7 +16,7 @@ function MobileMain({rating, activeRating, setActiveRating}: MobileProps) {
                 <img src={starIcon} aria-hidden="true" />
             </div>
 
-            <h1 className="text-white text-2xl font-semibold">How did we do?</h1>
+            <h1 className="text-white text-3xl font-semibold">How did we do?</h1>
 
             <p className="text-slate-400 text-sm">
                 Please let us know how we did with your support request. All feedback is appreciated
@@ -25,19 +27,29 @@ function MobileMain({rating, activeRating, setActiveRating}: MobileProps) {
                 {
                     rating.map((item, index) => {
                         return (
-                            <button key={index} onClick={() => setActiveRating(item)} className={`[ size-10 ] [ rounded-full ] [ ${activeRating === item ? 'text-black bg-orange-500' : 'text-white hover:text-black bg-grey-500 hover:bg-white'} ]`}>{item}</button>
+                            <Button key={index} onClick={() => setActiveRating(item)} className={`[ size-10 ] [ rounded-full ] [ ${activeRating === item ? 'text-black bg-orange-500' : 'text-white hover:text-black bg-grey-500 hover:bg-white'} ]`}>{item}</Button>
                         )
                     })
                 }
             </div>
 
-            <button className="[ uppercase font-bold hover:text-black ] [ bg-orange-500 hover:bg-white ] [ rounded-full ] [ p-3 ]">Submit</button>
-
-            <div className="hidden aria-hidden">
-                Thank you!
-                We appreciate you taking the time to give a rating. If you ever need more support,
-                don’t hesitate to get in touch!
-            </div>
+            <DialogTrigger>
+                <Button className="[ uppercase font-bold hover:text-black ] [ bg-orange-500 hover:bg-white ] [ rounded-full ] [ p-3 ]">Submit</Button>
+                <Modal className="[ bg-grey-900 ] [ rounded-xl ] [ p-5 ] absolute top-0 bottom-0 left-0 right-0 m-auto w-83.75 h-86" isDismissable>
+                    <Dialog className="[ flex flex-col gap-5 items-center ] text-center">
+                        <div className="img-container">
+                            <img src={illustrationThankYou} aria-hidden="true" />
+                        </div>
+                        <p className="text-orange-500 bg-grey-500 rounded-full p-2 text-sm w-fit">You selected {activeRating} out of 5</p>
+                        <h2 className="text-white text-2xl font-semibold">Thank you!</h2>
+                        <p className="text-slate-400 text-sm">
+                            We appreciate you taking the time to give a rating. If you ever need more support,
+                            don’t hesitate to get in touch!
+                        </p>
+                    </Dialog>
+                </Modal>
+            </DialogTrigger>
+            
         </div>
     )
 }
