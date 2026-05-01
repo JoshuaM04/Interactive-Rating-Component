36fa33933a8317a25f6260fae1b2c4257bbef68c
Viper <pruncle21@gmail.com>
2026-04-30 20:11:46 -0500

    Set the foundation for the HTML structure of the webpage, added Tailwind CSS import, added a components directory, added functional components for each body of the webpage for mobile layouts, and applied styling to the content within the webpage so its aligned appropriately.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,16 @@
+function MobileFooter() {
+    return (
+        <div>
+            <p>Challenge by <a className="font-semibold text-blue-500" href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a></p>
+            <p>Coded by <a className="font-semibold text-blue-500" href="#">Joshua Martinez</a></p>
+        </div>
+    )
+}
+
 export default function Footer() {
     return (
-        <>
-        </>
+        <footer className="fixed bottom-0 text-center text-white p-5">
+            <MobileFooter />
+        </footer>
     )
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,54 @@
-export default function Main() {
+import { useState } from 'react';
+
+interface MobileProps {
+    rating: Array<number>;
+    activeRating: boolean;
+    setActiveRating: Function;
+}
+
+function MobileMain({rating, activeRating, setActiveRating}: MobileProps) {
     return (
-        <>
-        </>
+        <div className="flex flex-col gap-5 bg-gray-600 rounded-xl p-5">
+            <h1 className="text-white font-semibold text-2xl">How did we do?</h1>
+
+            <p>
+                Please let us know how we did with your support request. All feedback is appreciated
+                to help us improve our offering!
+            </p>
+
+            <div className="flex justify-around">
+                {
+                    rating.map((item, index) => {
+                        return (
+                            <button key={index}>{item}</button>
+                        )
+                    })
+                }
+            </div>
+
+            <button>Submit</button>
+
+            <div className="hidden aria-hidden">
+                Thank you!
+                We appreciate you taking the time to give a rating. If you ever need more support,
+                don’t hesitate to get in touch!
+            </div>
+        </div>
+    )
+}
+
+
+export default function Main() {
+    const rating = [1, 2, 3, 4, 5];
+    const [activeRating, setActiveRating] = useState(false);
+
+    return (
+        <main>
+            <MobileMain 
+                rating={rating}
+                activeRating={activeRating}
+                setActiveRating={setActiveRating}
+            />
+        </main>
     )
 }
