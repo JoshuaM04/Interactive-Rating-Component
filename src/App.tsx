91264609c0bab31b8d825e99164b7f4228652c81
Viper <pruncle21@gmail.com>
2026-04-30 20:56:17 -0500

    Added styling to the main container and utilized a state variable to determine the active rating based on user input. Utilized conditional rendering for styling of the rating buttons. Adjusted the alignment of content and created custom utility classes via the @theme directive in App.css for the color scheme of the webpage to be more aligned with the base design.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Footer from './components/Footer.tsx';
 
 export default function App() {
   return (
-    <div className="[ parent_container ] [ flex flex-col justify-center items-center ] [ p-5 ] [ bg-gray-900 ] [ box-sizing ][ min-h-dvh ]">
+    <div className="[ parent_container ] [ flex flex-col justify-center items-center ] [ p-5 ] [ bg-grey-950 ] [ box-sizing ][ min-h-dvh ]">
       <Main />
       <Footer />
     </div>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,22 @@
+import starIcon from '../assets/images/icon-star.svg';
 import { useState } from 'react';
 
 interface MobileProps {
     rating: Array<number>;
-    activeRating: boolean;
+    activeRating: number;
     setActiveRating: Function;
 }
 
 function MobileMain({rating, activeRating, setActiveRating}: MobileProps) {
     return (
-        <div className="flex flex-col gap-5 bg-gray-600 rounded-xl p-5">
-            <h1 className="text-white font-semibold text-2xl">How did we do?</h1>
+        <div className="[ flex flex-col gap-5 ] [ bg-grey-900 ] [ rounded-xl ] [ p-5 ] [ w-83.75 ]">
+            <div className="[ img-container ] [ bg-grey-500 ] [ rounded-full ] [ p-3 ] [ size-fit ]">
+                <img src={starIcon} aria-hidden="true" />
+            </div>
 
-            <p>
+            <h1 className="text-white text-2xl font-semibold">How did we do?</h1>
+
+            <p className="text-slate-400 text-sm">
                 Please let us know how we did with your support request. All feedback is appreciated
                 to help us improve our offering!
             </p>
@@ -20,13 +25,13 @@ function MobileMain({rating, activeRating, setActiveRating}: MobileProps) {
                 {
                     rating.map((item, index) => {
                         return (
-                            <button key={index}>{item}</button>
+                            <button key={index} onClick={() => setActiveRating(item)} className={`[ size-10 ] [ rounded-full ] [ ${activeRating === item ? 'text-black bg-orange-500' : 'text-white hover:text-black bg-grey-500 hover:bg-white'} ]`}>{item}</button>
                         )
                     })
                 }
             </div>
 
-            <button>Submit</button>
+            <button className="[ uppercase font-bold hover:text-black ] [ bg-orange-500 hover:bg-white ] [ rounded-full ] [ p-3 ]">Submit</button>
 
             <div className="hidden aria-hidden">
                 Thank you!
@@ -40,7 +45,7 @@ function MobileMain({rating, activeRating, setActiveRating}: MobileProps) {
 
 export default function Main() {
     const rating = [1, 2, 3, 4, 5];
-    const [activeRating, setActiveRating] = useState(false);
+    const [activeRating, setActiveRating] = useState(0);
 
     return (
         <main>
